4420f2867e0eab03c61f2aa4b15c7402d6fd21df
Anoop Grover <iamgroveranoop.1979@gmail.com>
2025-08-27 00:06:31 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Home from "./components/Home"
 import About from "./components/About"
+import Projects from "./components/Projects"
+import Services from "./components/Services"
+import Certifications from "./components/Certifications"
+import Testimonials from "./components/Testimonials"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
     <div>
       <Home />
       <About />
-      {/* Next: Projects, Services, Certifications, Testimonials, Contact, Footer */}
+      <Projects />
+      <Services />
+      <Certifications />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </div>
   )
 }
