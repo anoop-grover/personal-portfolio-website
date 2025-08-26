dae988c6055288038b3546c185fe7e41231e8081
Anoop Grover <iamgroveranoop.1979@gmail.com>
2025-08-27 00:06:09 +0530

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-900 text-white py-6 text-center">
+      <p>© {new Date().getFullYear()} Anoop Grover. All rights reserved.</p>
+    </footer>
+  )
+}
