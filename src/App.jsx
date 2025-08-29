8bda8f94f933320d28dcd2add1df7f208bb703d4
Anoop Grover <iamgroveranoop.1979@gmail.com>
2025-08-29 08:14:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import Hero from "./components/Home";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Services from "./components/Services";
